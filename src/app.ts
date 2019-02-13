a86191c3f995d62cf831b3d9299577d849a2fbdc
lingxi <lingxi@jfjun.com>
2019-02-13 20:58:55 +0800

    2.0 将计算器组件抽出成ts文件

--- src/app.ts
+++ src/app.ts
@@ -1,66 +1,11 @@
 import Vue from 'vue';
-
+import MyCaculate from './Caculate';
 new Vue({
     el: "#app",
     template: `
-        <div>
-        <input type="number" v-model="a">
-        <select name="s"  @change=selectType>
-            <option :value="option.type" v-for="option in options"> {{option.type}} </option>
-        </select>
-        <input type="number" v-model="b"> 
-        <span>=</span>
-        <span>{{result}}</span>
-        <input type="button" value='计算' @click="caculate">
-        </div>
+       <my-caculate></my-caculate>
     `,
-    created() {
-        this.couponSelected = this.options[0].type;
-    },
-    data: {
-        result: 0,
-        a: 0,
-        b: 0,
-        type: '+',
-        options: [
-            {
-                type: '+',
-                name: "加"
-            },
-            {
-                type: '-',
-                name: "减"
-            },
-            {
-                type: '*',
-                name: "乘"
-            },
-            {
-                type: '/',
-                name: "除"
-            }
-        ]
-    },
-    methods: {
-        // 计算
-        caculate() {
-            switch (this.type) {
-                case '+':
-                    this.result = parseFloat(this.a) + parseFloat(this.b);
-                    break;
-                case '-':
-                    this.result = parseFloat(this.a) - parseFloat(this.b);
-                    break;
-                case '*':
-                    this.result = parseFloat(this.a) * parseFloat(this.b);
-                    break;
-                case '/':
-                    this.result = parseFloat(this.a) / parseFloat(this.b);
-                    break;
-            }
-        },
-        selectType(target) {
-            this.type = target.target.value;
-        }
+    components:{
+        MyCaculate
     }
 })
